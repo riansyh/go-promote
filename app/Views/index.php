f91754761f9720435b9f15abb735b5230b8d5ec1
Rian Febriansyah <rianfebriansyah22@gmail.com>
2020-11-10 22:31:26 +0700

    Change into CI 4 project

---
+++ app/Views/index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css" media="all" rel="stylsheet" type="text/css">
+    <script src="https://kit.fontawesome.com/a2c3b56892.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap">
+    <link rel="preload" href="https://fonts.gstatic.com/s/worksans/v5/QGYsz_wNahGAdqQ43Rh_fKDptfpA4Q.woff2" as="font">
+    <link rel="preload" href="https://fonts.gstatic.com/s/worksans/v5/QGYpz_wNahGAdqQ43Rh3x4X8mNhNy_r-Kw.woff2" as="font">
+    <!-- END CSS -->
+    <title>GoPromote</title>
+</head>
+
+<body>
+    <div class="kepala">
+        <header>
+            <a href="/" class="Logo"><img src="img/logo.png" alt=""></a>
+            <ul class="satu">
+                <li>
+                    <div class="home in">
+                        <a href="#" class="inti">Home</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="in">
+                        <a href="#produk" class="inti">Product</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="in">
+                        <a href="#review" class="inti">Testimoni</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="in">
+                        <a href="#bawah" class="inti">Contact</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="topdetil">
+                        <a href="#" class="detil">Detail</a>
+                        <i class="fas fa-chevron-down"></i>
+                        <ul class="intinya">
+                            <div>
+                                <li><a href="#">Syarat & ketentuan</a></li>
+                            </div>
+                            <div>
+                                <li><a href="#">Biodata Pembuat</a></li>
+                            </div>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <div class="loggin"><a href="#" class="login"></a></div>
+                </li>
+                <!-- <li><div class="register"><a href="#" class="regis inti">Logout</a></div></li> -->
+                <li>
+                    <div class="loggin"><a href="#" class="login">Login</a></div>
+                </li>
+                <li>
+                    <div class="register"><a href="register" class="regis inti">Register</a></div>
+                </li>
+            </ul>
+    </div>
+    </header>
+    </div>
+    <main>
+        <div class="container">
+            <div class="jmbtrn">
+                <div class="text">
+                    <p class="kata1">Promosikan Usahamu, <br> Tingkatkan Peluangmu!</h1>
+                        <p class="text-awal">Bingung nyari tempat untuk promosikan usahamu? <br> atau mungkin promosinya gitu-gitu aja? <br>
+                            <span class="bold">GoPromote solusinya!</span> Promo sikan usahamu disini <br>
+                            dan dapatkan keuntungan yang melimpah </p>
+                </div>
+                <div class="fotoppm"></div>
+            </div>
+
+            <div class="intro">
+                <h1>Apa Itu GoPromote?</h1>
+                <p>GoPromote merupakan sebuah layanan yang berfungsi untuk menawarkan jasa promosi berbayar (paid promote) dengan berbagai
+                    varian paket yang ditawarkan sehingga kamu bisa dengan mudah mempromosikan produk/usaha yang dimilikimu kepada para
+                    penguna sosial media</p>
+            </div>
+
+            <div class="alasan">
+                <h1>Kenapa Harus Memilih Kami?</h1>
+                <p>Karena dengan kamu menggunakan jasa paid promote kami, usaha yang kamu promosikan akan dilirik oleh banyak orang dan itu membuat usahamu bisa lebih berkembang</p>
+            </div>
+
+            <!-- Paket -->
+            <h1 class="paket" id="produk">Pilihan Paket</h1>
+            <p class="paket2">Kami menyediakan beberapa pilihan paket paid promote</p>
+            <div class="product">
+                <div class="psatu">
+                    <h1 class="namap">Bronze</h1>
+                    <h1>Rp.15.000</h2>
+                        <ul>
+                            <li>1 Day Keep</li>
+                            <li>1x Feed</li>
+                            <li>2x Instastory</li>
+                        </ul>
+                </div>
+
+                <div class="pdua">
+                    <h1 class="namap">Silver</h1>
+                    <h1>Rp.25.000</h2>
+                        <ul>
+                            <li>2 Day Keep</li>
+                            <li>1x Feed</li>
+                            <li>2x Instastory</li>
+                        </ul>
+                </div>
+
+                <div class="ptiga">
+                    <h1 class="namap">Gold</h1>
+                    <h1>Rp.50.000</h2>
+                        <ul>
+                            <li>5 Day Keep</li>
+                            <li>2x Feed</li>
+                            <li>4x Instastory</li>
+                        </ul>
+                </div>
+
+                <div class="pempat">
+                    <h1 class="namap">Platinum</h1>
+                    <h1>Rp.60.000</h2>
+                        <ul>
+                            <li>7 Day Keep</li>
+                            <li>3x Feed</li>
+                            <li>5x Instastory</li>
+                        </ul>
+                </div>
+
+                <div class="tPaket">
+                    <h1>Beli</h1>
+                    </a>
+                </div>
+                <div class="pattern"></div>
+            </div>
+
+
+            <!-- Review -->
+            <div class="rvw" id="review">
+                <h1>Testimoni</h1>
+            </div>
+            <p class="rvw2">Berikut merupakan beberapa testimoni tentang produk kami</p>
+            <div class="review">
+                <div class="r1">
+                    <a href="#"></a>
+                </div>
+
+                <div class="r2">
+                    <a href="#"></a>
+                </div>
+
+                <div class="r3">
+                    <a href="#">
+                        <r/a> </div> <div class="pattern">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="bawah" id="bawah">
+        <div class="container2">
+            <div class="foto"><a href="pembuat.php">
+                    <p>Our Team</p>
+                    <div class="fotori">
+                        <h3>Rian Febriansyah</h3>
+                    </div>
+                    <div class="fotorz">
+                        <h3>Rizal Herliansyah Hidayat</h3>
+                    </div>
+                    <div class="fotoid">
+                        <h3>Indra Kurniawan</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="container1">
+            <div class="middle">
+                <a class="btn" href="#">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a class="btn" href="#">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a class="btn" href="#">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a class="btn" href="#">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a class="btn" href="#">
+                    <i class="fab fa-line"></i>
+                </a>
+                <br>
+                <p>@ 2020 - All right reserved</p>
+            </div>
+        </div>
+    </footer>
+    <!-- Akhir Footer -->
+
+    <!-- Script -->
+    <script type="text/javascript" src="../JS/main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
