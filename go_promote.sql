921e43978caa80cb39c11c75912e1e9997120a21
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2020-12-08 21:06:48 +0700

    commit

--- go_promote.sql
+++ go_promote.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Des 2020 pada 10.47
+-- Waktu pembuatan: 08 Des 2020 pada 14.51
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.5
 
@@ -42,17 +42,7 @@ CREATE TABLE `jadwalpp` (
 --
 
 INSERT INTO `jadwalpp` (`tgl_pp`, `id_transaksi`, `id_paket`, `username`, `tgl_selesai`, `paket`, `status`) VALUES
-('2020-11-19', 12357, 3, 'riansyah22', '2020-11-24', 'Gold', 'Selesai'),
-('2020-11-05', 12358, 1, 'riansyh22', '2020-11-06', 'Bronze', 'Selesai'),
-('2020-05-09', 12359, 1, 'riansyah22', '2020-05-10', 'Bronze', 'Belum'),
-('2020-12-02', 12360, 1, 'rizalh', '2020-12-03', 'Bronze', 'Selesai'),
-('2020-12-05', 12362, 4, 'evosroar', '2020-12-12', 'Platinum', 'Selesai'),
-('2020-12-24', 12363, 1, 'botolkecap', '2020-12-25', 'Bronze', 'Proses'),
-('2020-12-09', 12364, 4, 'rizalh', '2020-12-16', 'Platinum', 'Proses'),
-('2020-12-09', 12367, 3, 'rizalh', '2020-12-14', 'Gold', 'Selesai'),
-('2020-12-09', 12368, 3, 'rizalh', '2020-12-14', 'Gold', 'Proses'),
-('2020-12-16', 12369, 2, 'rizalh', '2020-12-18', 'Silver', 'Proses'),
-('2020-12-17', 12370, 3, 'rizalh', '2020-12-22', 'Gold', 'Proses');
+('2020-12-17', 12371, 4, 'rizalh', '2020-12-24', 'Platinum', 'Proses');
 
 -- --------------------------------------------------------
 
@@ -101,18 +91,7 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id_transaksi`, `metode`, `tgl_transaksi`, `id_paket`, `tgl_pp`, `username`, `caption`, `foto`, `status`, `jasa_desain`) VALUES
-(12357, 'LinkAja', '2020-11-29', 3, '2020-11-19', 'riansyah22', 'qsqs', '5fc3cac69d9f0.jpeg', 'Proses', 1),
-(12358, 'GoPay', '2020-11-30', 1, '2020-11-05', 'riansyh22', 'nbjkkjn', '5fc4fc571ee76.png', 'Proses', 1),
-(12359, 'OVO', '2020-11-12', 1, '2020-05-09', 'riansyah22', '', '', 'Belum', 0),
-(12360, 'OVO', '2020-12-01', 1, '2020-12-02', 'rizalh', '111', '5fc650ad04d49.png', 'Proses', 0),
-(12361, 'LinkAja', '2020-12-01', 4, '2020-12-02', 'rizalh', 'fsfsdf', '5fc653a351627.png', 'Proses', 1),
-(12362, 'LinkAja', '2020-12-01', 4, '2020-12-05', 'evosroar', 'aid romote ', '5fc6f0586de5d.png', 'Proses', 1),
-(12363, 'OVO', '2020-12-02', 1, '2020-12-24', 'botolkecap', 'asasas', '5fc7b7e8b214d.png', 'Proses', 1),
-(12364, 'OVO', '2020-12-03', 4, '2020-12-09', 'rizalh', '', '5fc9a00e596f3.png', 'Proses', 1),
-(12367, 'GoPay', '2020-12-08', 3, '2020-12-09', 'rizalh', 'sdasdsadsffd', '5fcf268da870e.png', 'Proses', 1),
-(12368, 'GoPay', '2020-12-08', 3, '2020-12-09', 'rizalh', 'liho', '5fcf28c8846a3.png', 'Proses', 1),
-(12369, 'Dana', '2020-12-08', 2, '2020-12-16', 'rizalh', 'grg', '5fcf29186c78f.png', 'Proses', 1),
-(12370, 'OVO', '2020-12-08', 3, '2020-12-17', 'rizalh', 'sw', '5fcf294baa544.png', 'Proses', 1);
+(12371, 'Dana', '2020-12-08', 4, '2020-12-17', 'rizalh', 'rizal asdashdlashdashjdhasjdhkjsahdkasd', '5fcf7d22415bb.png', 'Proses', 1);
 
 --
 -- Trigger `transaksi`
@@ -141,16 +120,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `level`) VALUES
-('botolkecap', '$2y$10$D6PhRDc70SNHCOroRXNPseiJaYOczGEZs/gatG0V18H8aWnmYbfn2', 1),
-('budiono', '$2y$10$4m0qMZvH83WmygLOQNUj8.utPWscCflmTWtDk9um2gUHSyB/ac17O', 1),
-('evosroar', '$2y$10$LxZUpDG2ut8iomxKDJYCJ.sqvWE.EmvXGul7zUUgTkCRQqKG71ody', 1),
-('gelasbotol', '$2y$10$iN8vaCExp7haJ1XasfcwMO062ZOmE3rrkt2x4bwj4EHhXBAjEmI3S', 1),
-('mystogan', '$2y$10$w46Q5YwHsmBgbYn7VxGACONXwgApFdcmeOYh52jDOwtss94airWGC', 1),
 ('riansyah22', '$2y$10$l7AsAZ5RjlpHGa7tFYjH4uk6LRnk8mOdFCm5Ll72xVe//1MGDP/eC', 1),
 ('riansyh22', '$2y$10$W5EJYyRxYh3O58uRg9yw2eAv/.8jTO0bKjFrcF5gLPGsCnrzanX6i', 1),
 ('rizalh', '$2y$10$EzqYznGw2xW2wcgL0ZjS3OSbyRjQpsRZuHbD6dMnwljMkbTafADLu', 1),
-('rizalhaha', '$2y$10$6XS8BQx0Ie24Y9QV.GCttuOMIebQqYX2Xs4.FdGwOw5jB6o7RRIFe', 2),
-('rizalhaji', '$2y$10$76VrZ/DYFw3wNNy2d0wLE.2djlUJNr0jPFQ/gHd7i1qZL5BOb5vyS', 1);
+('rizalh2001', '$2y$10$6XS8BQx0Ie24Y9QV.GCttuOMIebQqYX2Xs4.FdGwOw5jB6o7RRIFe', 2),
+('rizalmultimedia', '$2y$10$skiuSlU1Y0xONBajizoRiuN.5h0kCo6vjCPNqB/GgOONXi9BgsFvK', 1);
 
 -- --------------------------------------------------------
 
@@ -175,14 +149,9 @@ CREATE TABLE `users_data` (
 INSERT INTO `users_data` (`username`, `nama`, `no_hp`, `email`, `instagram`, `user_id`, `foto`) VALUES
 ('riansyah22', 'Rian', '089660254544', 'Rianfebriansyah22@gmail.com', 'rian', 9, ''),
 ('riansyh22', 'Rian Febriansyah', '089660254544', 'Rianfebriansyah22@gmail.com', 'riansyh', 10, ' '),
-('rizalhaha', 'Rizal herliansyah', '089651983134', 'rizalmultimedia@gmail.com', '@rizalhh4', 11, ' '),
-('rizalh', 'Rizal', '089651983134', 'rizalmultimedia2@gmail.com', '@rizalhajihidayat', 12, '5fcf29cc435a6.png'),
-('mystogan', 'rizal', '089651983134', 'rizalmultimeddia2@gmail.com', '@rizalhh', 13, '5fc6ee5587eaf.png'),
-('evosroar', 'rian febriansyah', '082121231312', 'rian@gmail.com', '@riansyh', 14, '5fc6efd343c74.png'),
-('rizalhaji', 'rizalhaha', '089651983134', 'rizalmultimedia2@gmail.com', '@rizalhaha', 15, ''),
-('gelasbotol', 'rizalhidayat', '2121212', 'rizalmultimedia2@gmail.com', '20012001', 16, '5fc78f5146924.png'),
-('botolkecap', 'Rizal', '2012', 'rizalmultimedia2@gmail.com', '20012', 17, NULL),
-('budiono', 'Rizal Hidayat', '082134543176', 'rizalmultimedia21@gmail.com', '@rizalh', 18, '5fcf4ab06465b.png');
+('rizalh2001', 'Rizal herliansyah', '089651983134', 'rizalmultimedia@gmail.com', '@rizalhh4', 11, ' '),
+('rizalh', 'Rizal', '089651983134', 'rizalmultimedia2@gmail.com', '@rizalhajihidayat', 12, '5fcf7d07d0a0d.png'),
+('rizalmultimedia', 'Rizal goku', '081712987182', 'Rizalmultimedia6@gmail.com', '@rizalhhhh', 19, '5fcf76e89d3b0.png');
 
 --
 -- Indexes for dumped tables
@@ -237,13 +206,13 @@ ALTER TABLE `paket`
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12371;
+  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12372;
 
 --
 -- AUTO_INCREMENT untuk tabel `users_data`
 --
 ALTER TABLE `users_data`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
